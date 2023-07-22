55f938c1b4923d882bc34d5469f46f04ab1cd90f
linx-235 <xyfernx@gmail.com>
2023-07-22 14:28:21 -0400

    checkpoint

--- Game/Assets/PlayerMovement.cs
+++ Game/Assets/PlayerMovement.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        float dirX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump")) {
+            rb.velocity = new Vector2(rb.velocity.x, 14f);
+        }
     }
 }
